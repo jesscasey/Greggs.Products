6756d4a7a67d7f77bf0086417dbe1acec119327e
Jess Casey <jessicacasey27@yahoo.com>
2025-09-03 19:42:03 +0100

    Add LatestItems method

--- Greggs.Products.Api/Program.cs
+++ Greggs.Products.Api/Program.cs
@@ -8,9 +8,15 @@ public class Program
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
+        using HttpResponseMessage response = await httpClient.GetAsync("/product");
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+    public IEnumerable<Product> LatestItems(IEnumerable<Product> products)
+    {
+        return products.OrderByDescending(p => p.AvailableFrom);
+    }
 }
