36bfdf3b6a8ce5d18bef4b4bdc5f6d73949611ab
Jess Casey <jessicacasey27@yahoo.com>
2025-09-03 20:04:45 +0100

    Add unit tests and test data

--- Greggs.Products.UnitTests/UnitTest1.cs
+++ Greggs.Products.UnitTests/UnitTest1.cs
@@ -5,9 +5,37 @@ namespace Greggs.Products.UnitTests;
 
 public class UnitTest1
 {
+    IEnumerable<Product> testProducts = [new Product
+            {
+                PriceInPounds = 1m,
+                Name = "Sausage Roll",
+                AvailableFrom = "2025-01-01"
+            },
+            new Product
+            {
+                PriceInPounds = 2m,
+                Name = "Veggie Bake",
+                AvailableFrom = "2025-01-02"
+            },
+            new Product
+            {
+                PriceInPounds = 3m,
+                Name = "Steak Bake",
+                AvailableFrom = "2025-01-03"
+            }
+        ].ToArray();
+
     [Fact]
-    public void Test1()
+    public void TestLatestItems()
     {
-        throw new NotImplementedException("We have no tests :-(");
+        var productList = new Program().LatestItems(testProducts);
+        Assert(productList.First().AvailableFrom = "2025-01-03");
+    }
+
+    [Fact]
+    public void TestToEuros()
+    {
+        var product = new Program().ToEuros(testProducts).First();
+        Assert(product.PriceInEuros = 1.11m);
     }
 }
